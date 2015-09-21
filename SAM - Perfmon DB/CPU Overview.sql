90d24e8227f824299d5525781cef3fc29f98edc8
samyan19 <samuel_yanzu@yahoo.co.uk>
2015-09-21 01:58:53 +0100

    Update CPU Overview.sql

--- SAM - Perfmon DB/CPU Overview.sql
+++ SAM - Perfmon DB/CPU Overview.sql
@@ -15,18 +15,12 @@ GO
 DECLARE @CountAboveThreshold INT
 DECLARE	@TotalCount INT
 
-SET @CountAboveThreshold=(SELECT COUNT(1)
-FROM dbo.CounterData
-WHERE CounterValue>75)
 
-SET @TotalCount=(SELECT COUNT(1) FROM dbo.CounterData)
-
-SELECT 
-		 cdt.countername,
-		MIN(CounterValue) AS MinValue,
-        MAX(CounterValue) AS MaxValue,
-        AVG(CounterValue) AS AvgValue,
-		CAST(@CountAboveThreshold AS DECIMAL(10,2))/CAST(@TotalCount AS DECIMAL (10,2))*100.0 AS 'PercentageAboveThreshold'
+/*
+select working set
+*/
+SELECT CounterName,CounterValue
+INTO #Dataset
 FROM    dbo.CounterDetails cdt
         INNER JOIN dbo.CounterData cd ON cdt.CounterID = cd.CounterID
         INNER JOIN dbo.DisplayToID d ON d.GUID = cd.GUID
@@ -34,7 +28,32 @@ WHERE
         --ObjectName in ('LogicalDisk','Processor','SQLServer:SQL Statistics')
         /*AND*/ cdt.CounterName IN( '% Processor Time')
         AND cdt.InstanceName = '_Total'
-GROUP BY CounterName
+
+/* find total count*/
+SET @TotalCount=(SELECT COUNT(1) FROM #Dataset)
+
+/*find count matching criteria*/
+SET @CountAboveThreshold=(SELECT COUNT(1) FROM #Dataset WHERE countervalue>75.0)
+
+
+/*return aggregates and results*/
+SELECT 
+		countername,
+		MIN(CounterValue) AS MinValue,
+        MAX(CounterValue) AS MaxValue,
+        AVG(CounterValue) AS AvgValue,
+		CAST(@CountAboveThreshold AS DECIMAL(10,2))/CAST(@TotalCount AS DECIMAL (10,2))*100.0 AS 'PercentAboveThreshold'
+FROM #Dataset
+GROUP BY CounterName;
+
+
+IF  EXISTS (SELECT OBJECT_ID('tempdb..#dataset'))
+	DROP TABLE #dataset;
+
+
+--26:1 DATA SIZE TO MEMORY
+--28% above threshold
+
 
 
 
