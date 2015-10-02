2577dadc00e0c28c7e3b9262ebfc3322a6b78e69
samyan19 <samuel_yanzu@yahoo.co.uk>
2015-10-02 11:32:20 +0100

    Create List Replication Details.sql

---
+++ SAM - Replication/List Replication Details.sql
@@ -0,0 +1,14 @@
+--2012
+
+EXEC distribution.dbo.sp_replmonitorhelpsubscription @publication_type = 0
+
+--Pre 2012
+
+EXEC distribution.dbo.sp_replmonitorhelpsubscription @publication_type = 0, @Publisher = 'RCSTVSQL2K83-01\ISQL2K801'
+
+
+/*
+
+
+
+*/
