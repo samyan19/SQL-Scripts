bc55f8db50bba7431640d51f24bf87cf01cfad9b
samyan19 <samuel_yanzu@yahoo.co.uk>
2016-02-08 13:56:41 +0000

    Update JOBS - JOBS and durations.sql

--- SAM - Jobs/JOBS - JOBS and durations.sql
+++ SAM - Jobs/JOBS - JOBS and durations.sql
@@ -1,5 +1,7 @@
-select distinct j.name,j.enabled,jh.run_status,jh.run_date,jh.run_time,convert(varchar(100),run_duration/10000) +'h '+convert(varchar(100),run_duration/100%100)+'m '+convert(varchar(100),run_duration%100) +'s' as duration
-from sysjobs j
-inner join sysjobhistory jh on j.job_id=jh.job_id
-where j.name='DBA Daily Backup - raCentrica'
-order by jh.run_date desc
+select distinct j.name,j.enabled,jh.run_status,dbo.agent_datetime(jh.run_date,jh.run_time)as run_datetime,convert(varchar(100),run_duration/10000) +'h '+convert(varchar(100),run_duration/100%100)+'m '+convert(varchar(100),run_duration%100) +'s' as duration 
+from sysjobs j 
+inner join sysjobhistory jh on j.job_id=jh.job_id 
+--inner join sysjobactivity ja on jh.instance_id=ja.job_history_id
+where j.name='HDSUpdateStatistics' and jh.step_id=0
+--group by j.job_id,j.name,j.enabled,jh.run_status,run_date,run_time,run_duration
+order by run_datetime desc 
