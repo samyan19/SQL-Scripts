dff8fb0fea840c64b02db3970ff09a70519c81b2
samyan19 <samuel_yanzu@yahoo.co.uk>
2016-02-09 09:41:42 +0000

    Update SEC-Find who created user.sql

--- SAM - Security/SEC-Find who created user.sql
+++ SAM - Security/SEC-Find who created user.sql
@@ -1,1 +1,20 @@
+USE [your_database];
+GO
 
+DECLARE
+    @trace_id INT,
+    @filename NVARCHAR(4000);
+
+SELECT @trace_id = id
+    FROM sys.traces
+    WHERE is_default = 1;
+
+SELECT @filename = CONVERT(NVARCHAR(4000), value)
+    FROM sys.fn_trace_getinfo(@trace_id)
+    WHERE property = 2;
+
+SELECT HostName, LoginName, StartTime
+    FROM sys.fn_trace_gettable(@filename, DEFAULT)
+    WHERE EventClass   = 109
+    AND DatabaseName   = N'your_database'
+    AND TargetUserName = N'bob'
