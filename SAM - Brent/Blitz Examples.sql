a8fa20a8f2fa6bf17b2782407108a10ac7b41eb3
samyan19 <samuel_yanzu@yahoo.co.uk>
2016-04-15 13:25:00 +0100

    Update Blitz Examples.sql

--- SAM - Brent/Blitz Examples.sql
+++ SAM - Brent/Blitz Examples.sql
@@ -20,3 +20,15 @@ exec dbo.sp_BlitzTrace @Action='stop'
 exec dbo.sp_BlitzTrace @Action='drop'
 
 
+/*==========================
+Sp_Blitz common example
+===========================*/
+
+EXEC [master].[dbo].[sp_Blitz]
+    @CheckUserDatabaseObjects = 1 ,
+    @CheckProcedureCache = 0 ,
+    @OutputType = 'TABLE' ,
+    @OutputProcedureCache = 0 ,
+    @CheckProcedureCacheFilter = NULL,
+    @CheckServerInfo = 1
+
