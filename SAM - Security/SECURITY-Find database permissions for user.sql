5e62dfccb7cccbd746658e5dc4b88f4a60b524f4
samyan19 <samuel_yanzu@yahoo.co.uk>
2016-07-20 15:12:15 +0100

    Create SECURITY-Find database permissions for user.sql

---
+++ SAM - Security/SECURITY-Find database permissions for user.sql
@@ -0,0 +1,12 @@
+DECLARE @DBuser_sql VARCHAR(4000)
+DECLARE @DBuser_table TABLE (DBName VARCHAR(200), UserName VARCHAR(250), LoginType VARCHAR(500), AssociatedRole VARCHAR(200))
+SET @DBuser_sql='SELECT ''?'' AS DBName,a.name AS Name,a.type_desc AS LoginType,c.name AS AssociatedRole 
+FROM [?].sys.database_principals a
+LEFT OUTER JOIN [?].sys.database_role_members b ON a.principal_id=b.member_principal_id
+LEFT OUTER JOIN [?].sys.database_principals c ON c.principal_id=b.role_principal_id
+WHERE a.sid NOT IN (0x01,0x00) AND a.sid IS NOT NULL AND a.type NOT IN (''C'') AND a.is_fixed_role <> 1 AND a.name NOT LIKE ''##%'' AND ''?'' NOT IN (''master'',''msdb'',''model'',''tempdb'') ORDER BY Name'
+INSERT @DBuser_table
+EXEC sp_MSforeachdb @command1=@dbuser_sql
+SELECT @@servername as ServerName,* FROM @DBuser_table
+where username in ('CLOSEBROTHERSGP\ROLE-D-QuantumPRODUsers')
+--and dbname in ('CAFBi','LettersDB') --order by dbname
