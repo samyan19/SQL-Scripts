5a6d072b99aa29512573c7c8bf14149e7425f5b8
samyan19 <samuel_yanzu@yahoo.co.uk>
2016-08-11 19:25:03 +0100

    Update sp_whoisactive - create log table.sql

--- SAM - sp_whoisactive/sp_whoisactive - create log table.sql
+++ SAM - sp_whoisactive/sp_whoisactive - create log table.sql
@@ -1,48 +1,14 @@
-USE [zzSQLServerAdmin]
-GO
+DECLARE @destination_table VARCHAR(4000) ;
+SET @destination_table = 'WhoIsActive' ;
 
-/****** Object:  Table [dbo].[WhoIsActive]    Script Date: 02/04/2015 11:57:13 ******/
-SET ANSI_NULLS ON
-GO
+DECLARE @schema VARCHAR(4000) ;
+EXEC sp_WhoIsActive
+@get_transaction_info = 1,
+@get_plans = 1,
+@return_schema = 1,
+@schema = @schema OUTPUT ;
 
-SET QUOTED_IDENTIFIER ON
-GO
+SET @schema = REPLACE(@schema, '<table_name>', @destination_table) ;
 
-SET ANSI_PADDING ON
-GO
-
-CREATE TABLE [dbo].[WhoIsActive](
-	[dd hh:mm:ss.mss] [varchar](8000) NULL,
-	[session_id] [smallint] NOT NULL,
-	[sql_text] [xml] NULL,
-	[login_name] [nvarchar](128) NOT NULL,
-	[wait_info] [nvarchar](4000) NULL,
-	[tran_log_writes] [nvarchar](4000) NULL,
-	[CPU] [varchar](30) NULL,
-	[tempdb_allocations] [varchar](30) NULL,
-	[tempdb_current] [varchar](30) NULL,
-	[blocking_session_id] [smallint] NULL,
-	[reads] [varchar](30) NULL,
-	[writes] [varchar](30) NULL,
-	[physical_reads] [varchar](30) NULL,
-	[query_plan] [xml] NULL,
-	[used_memory] [varchar](30) NULL,
-	[status] [varchar](30) NOT NULL,
-	[tran_start_time] [datetime] NULL,
-	[open_tran_count] [varchar](30) NULL,
-	[percent_complete] [varchar](30) NULL,
-	[host_name] [nvarchar](128) NULL,
-	[database_name] [nvarchar](128) NULL,
-	[program_name] [nvarchar](128) NULL,
-	[start_time] [datetime] NOT NULL,
-	[login_time] [datetime] NULL,
-	[request_id] [int] NULL,
-	[collection_time] [datetime] NOT NULL
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-
-GO
-
-SET ANSI_PADDING OFF
-GO
-
-
+PRINT @schema
+EXEC(@schema) ;
