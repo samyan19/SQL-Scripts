c5192e02b5a705dbf917d49a5e0dde476f3f851a
samyan19 <samuel_yanzu@yahoo.co.uk>
2016-08-19 13:18:51 +0100

    Create Find who took backup.sql

---
+++ SAM - BACKUP/Find who took backup.sql
@@ -0,0 +1,6 @@
+--***************************************************************
+--* 1. List location of the last backup file 
+--* 2. Once location is found run the following to get information
+--****************************************************************
+
+RESTORE HEADERONLY FROM DISK = 'd:\bu\mm.bak' ;
