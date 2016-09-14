a7da838689586aae7cacc275dcb55a3091a9660f
samyan19 <samuel_yanzu@yahoo.co.uk>
2016-09-14 01:00:29 +0100

    Update DISK - io_virtual_file_stats.sql

--- SAM - Disk/DISK - io_virtual_file_stats.sql
+++ SAM - Disk/DISK - io_virtual_file_stats.sql
@@ -4,7 +4,46 @@
 >15ms bad
 */
 
+--PAUL RANDAL SCRIPT
+SELECT
+    [ReadLatency] =
+        CASE WHEN [num_of_reads] = 0
+            THEN 0 ELSE ([io_stall_read_ms] / [num_of_reads]) END,
+    [WriteLatency] =
+        CASE WHEN [num_of_writes] = 0
+            THEN 0 ELSE ([io_stall_write_ms] / [num_of_writes]) END,
+    [Latency] =
+        CASE WHEN ([num_of_reads] = 0 AND [num_of_writes] = 0)
+            THEN 0 ELSE ([io_stall] / ([num_of_reads] + [num_of_writes])) END,
+    [AvgBPerRead] =
+        CASE WHEN [num_of_reads] = 0
+            THEN 0 ELSE ([num_of_bytes_read] / [num_of_reads]) END,
+    [AvgBPerWrite] =
+        CASE WHEN [num_of_writes] = 0
+            THEN 0 ELSE ([num_of_bytes_written] / [num_of_writes]) END,
+    [AvgBPerTransfer] =
+        CASE WHEN ([num_of_reads] = 0 AND [num_of_writes] = 0)
+            THEN 0 ELSE
+                (([num_of_bytes_read] + [num_of_bytes_written]) /
+                ([num_of_reads] + [num_of_writes])) END,
+    LEFT ([mf].[physical_name], 2) AS [Drive],
+    DB_NAME ([vfs].[database_id]) AS [DB],
+    [mf].[physical_name]
+FROM
+    sys.dm_io_virtual_file_stats (NULL,NULL) AS [vfs]
+JOIN sys.master_files AS [mf]
+    ON [vfs].[database_id] = [mf].[database_id]
+    AND [vfs].[file_id] = [mf].[file_id]
+-- WHERE [vfs].[file_id] = 2 -- log files
+-- ORDER BY [Latency] DESC
+-- ORDER BY [ReadLatency] DESC
+ORDER BY [WriteLatency] DESC;
+GO
 
+
+/*****************
+Older script
+*****************/
 
 SELECT a.io_stall, a.io_stall_read_ms, a.io_stall_write_ms, a.num_of_reads, 
 a.num_of_writes, a.io_stall_read_ms/a.num_of_reads AS msperread,a.io_stall_write_ms/a.num_of_writes AS msperwrite,
