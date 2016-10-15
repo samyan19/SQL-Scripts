ec7d304529cd3277b3776444e382dc97c7326d09
samyan19 <samuel_yanzu@yahoo.co.uk>
2016-10-15 05:49:04 +0100

    Update BACKUP-Backup mulitple databases.sql

--- SAM - BACKUP/BACKUP-Backup mulitple databases.sql
+++ SAM - BACKUP/BACKUP-Backup mulitple databases.sql
@@ -1,4 +1,4 @@
-DECLARE @name VARCHAR(50) -- database name  
+DECLARE @name VARCHAR(256) -- database name  
 DECLARE @path VARCHAR(256) -- path for backup files  
 DECLARE @fileName VARCHAR(256) -- filename for backup  
 DECLARE @fileDate VARCHAR(20) -- used for file name
