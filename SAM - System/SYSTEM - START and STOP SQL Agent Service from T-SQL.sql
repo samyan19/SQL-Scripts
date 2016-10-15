5a842a57e001aba8db97c0db8e330917d0b4ccaa
samyan19 <samuel_yanzu@yahoo.co.uk>
2016-10-15 07:26:55 +0100

    Update SYSTEM - START and STOP SQL Agent Service from T-SQL.sql

--- SAM - System/SYSTEM - START and STOP SQL Agent Service from T-SQL.sql
+++ SAM - System/SYSTEM - START and STOP SQL Agent Service from T-SQL.sql
@@ -1,12 +1,14 @@
-/* http://www.sanjaykumar.us/index.php/start-stop-sql-agent-service-using-tsql-commands/ */
+/* https://www.mssqltips.com/sqlservertip/2036/monitor-start-and-stop-sql-server-services-using-xpservicecontrol/ */
 
---CHeck status
-EXECUTE xp_servicecontrol 'querystate', 'MSSQLServerOlapService'
-
--- Start
-EXEC xp_servicecontrol N'START',N'SQLServerAGENT'
-GO
- 
--- STOP 
-EXEC xp_servicecontrol N'STOP',N'SQLServerAGENT'
-GO
+--See below example to check the status of SQL Services
+EXEC xp_servicecontrol N'querystate',N'MSSQLServer'
+EXEC xp_servicecontrol N'querystate',N'SQLServerAGENT'
+EXEC xp_servicecontrol N'querystate',N'msdtc'
+EXEC xp_servicecontrol N'querystate',N'sqlbrowser'
+EXEC xp_servicecontrol N'querystate',N'MSSQLServerOLAPService'
+EXEC xp_servicecontrol N'querystate',N'ReportServer'
+--See below example to start/stop service using SSMS
+EXEC xp_servicecontrol N'stop',N'SQLServerAGENT'
+EXEC xp_servicecontrol N'start',N'SQLServerAGENT'
+--See below example to check non-SQL Service
+EXEC xp_servicecontrol querystate, DHCPServer
