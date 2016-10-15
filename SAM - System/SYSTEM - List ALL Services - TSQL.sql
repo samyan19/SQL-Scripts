bb0b39d20a5162b8976998899ca39137a06b76c3
samyan19 <samuel_yanzu@yahoo.co.uk>
2016-10-15 07:55:28 +0100

    Create SYSTEM - List ALL Services - TSQL.sql

---
+++ SAM - System/SYSTEM - List ALL Services - TSQL.sql
@@ -0,0 +1,6 @@
+/*
+https://www.simple-talk.com/sql/database-administration/the-posh-dba-solutions-using-powershell-and-sql-server/
+*/
+
+
+xp_cmdshell 'powershell.exe -noprofile get-service -name *sql*'
