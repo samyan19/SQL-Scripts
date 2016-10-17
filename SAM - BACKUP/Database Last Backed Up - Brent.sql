01b708348e3f677344b1a65e3172bbf35df151c6
samyan19 <samuel_yanzu@yahoo.co.uk>
2016-10-17 10:40:33 +0100

    Update Database Last Backed Up - Brent.sql

--- SAM - BACKUP/Database Last Backed Up - Brent.sql
+++ SAM - BACKUP/Database Last Backed Up - Brent.sql
@@ -9,8 +9,7 @@ GROUP BY d.name
 ORDER BY 2
 */
 
-
-SELECT a.name, a.last_backup_finish_date,bmf.physical_device_name
+SELECT distinct a.name, a.last_backup_finish_date,bmf.physical_device_name
 from msdb.dbo.backupmediafamily bmf 
 join 
 (
@@ -18,6 +17,8 @@ SELECT  d.name, MAX(b.backup_finish_date) AS last_backup_finish_date, MAX(b.medi
 FROM    master.sys.databases d WITH (NOLOCK)
 LEFT OUTER JOIN msdb.dbo.backupset b WITH (NOLOCK) ON d.name = b.database_name AND b.type = 'D'
 WHERE d.name <> 'tempdb'
+/*Uncomment below for last networker backup*/
+--and b.user_name='NT AUTHORITY\SYSTEM'
 GROUP BY d.name
 ) a on bmf.media_set_id=a.last_media_set_id
 order by 1
