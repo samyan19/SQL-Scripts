73f139c7e72b23d394ef38528cdb3e841b7b7ecb
samyan19 <samuel_yanzu@yahoo.co.uk>
2016-10-24 16:54:59 +0100

    Update RESTORE - Find Last Restore Date.sql

--- SAM - RESTORE/RESTORE - Find Last Restore Date.sql
+++ SAM - RESTORE/RESTORE - Find Last Restore Date.sql
@@ -13,9 +13,11 @@ SELECT  distinct [rs].[destination_database_name] ,
         [bs].[backup_start_date] ,
         [bs].[backup_finish_date] ,
         [bs].[database_name] AS [source_database_name] ,
-        [bmf].[physical_device_name] AS [backup_file_used_for_restore]
+        [bmf].[physical_device_name] AS [backup_file_used_for_restore],
+        rs.user_name
 FROM    msdb..restorehistory rs
         INNER JOIN msdb..backupset bs ON [rs].[backup_set_id] = [bs].[backup_set_id]
         INNER JOIN msdb..backupmediafamily bmf ON [bs].[media_set_id] = [bmf].[media_set_id]
-WHERE rs.destination_database_name = 'CBL_MDS_SIT'
+WHERE rs.destination_database_name in ('P2_Current','P2_CRMIRefData','P2_CRMIMirror')
 ORDER BY [rs].[restore_date] DESC
+
