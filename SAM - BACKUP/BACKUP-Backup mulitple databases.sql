8c017c666c1afba9b370769bb1f4ac9641f33e77
samyan19 <samuel_yanzu@yahoo.co.uk>
2016-11-17 11:52:48 +0000

    Update BACKUP-Backup mulitple databases.sql

--- SAM - BACKUP/BACKUP-Backup mulitple databases.sql
+++ SAM - BACKUP/BACKUP-Backup mulitple databases.sql
@@ -1,16 +1,18 @@
 DECLARE @name VARCHAR(256) -- database name  
 DECLARE @path VARCHAR(256) -- path for backup files  
 DECLARE @fileName VARCHAR(256) -- filename for backup  
-DECLARE @fileDate VARCHAR(20) -- used for file name
+DECLARE @fileDate VARCHAR(256) -- used for file name
+DECLARE @Now DATETIME = CURRENT_TIMESTAMP
+--Use smalldatetime if you do not want seconds
  
 -- specify database backup directory
-SET @path = '\\pdc2bkp001\Treasury_DR\'+@@SERVERNAME+'\'  
-
+SET @path = '\\pdc1bkp001\CCT_Backups\'+@@SERVERNAME+'\'  
 -- Create backup path
 exec xp_create_subdir @path
  
 -- specify filename format
-SELECT @fileDate = CONVERT(VARCHAR(20),GETDATE(),112) 
+--SELECT @fileDate = CONVERT(VARCHAR(20),GETDATE(),112) 
+SELECT @fileDate=REPLACE(REPLACE(REPLACE(CONVERT(VARCHAR(32),@Now,120), '-',''), ' ','_'),':','')
  
 DECLARE db_cursor CURSOR FOR  
 --SELECT name 
