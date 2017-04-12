4e760c5a6050c33a726c752172d8b98f59e69b17
samyan19 <samuel_yanzu@yahoo.co.uk>
2017-04-12 17:16:53 +0100

    Update DATABASE-Set all databases to SIMPLE Recovery.sql

--- SAM - Database/DATABASE-Set all databases to SIMPLE Recovery.sql
+++ SAM - Database/DATABASE-Set all databases to SIMPLE Recovery.sql
@@ -1,6 +1,7 @@
-Select 'ALTER DATABASE '+name+' SET RECOVERY SIMPLE WITH NOWAIT;'
+Select 'ALTER DATABASE ['+name+'] SET RECOVERY SIMPLE WITH ROLLBACK IMMEDIATE;'
 from sys.databases
 where recovery_model_desc = 'FULL'
 and database_id>4
+
 
 /* Then take output and run in SSMS*/
