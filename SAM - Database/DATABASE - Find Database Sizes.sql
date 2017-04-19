49497356658d4fd0267205007fdd289d4fb69c5d
samyan19 <samuel_yanzu@yahoo.co.uk>
2017-04-19 12:45:23 +0100

    Update DATABASE - Find Database Sizes.sql

--- SAM - Database/DATABASE - Find Database Sizes.sql
+++ SAM - Database/DATABASE - Find Database Sizes.sql
@@ -12,3 +12,25 @@ where b.type_desc='ROWS'
 group by a.name,a.compatibility_level
 ,b.database_id
 order by Size_MB desc
+
+
+
+with CTE as
+(
+select @@servername as servername, 
+a.name, 
+a.compatibility_level,
+b.database_id [database_id],
+SUM(((size*8)/1024)) [Size_MB],
+count(b.file_id) as file_count
+FROM sys.databases a
+join sys.master_files b on a.database_id=b.database_id
+where b.type_desc='ROWS'
+group by a.name,a.compatibility_level
+,b.database_id
+--order by Size_MB desc
+)
+select sum(Size_MB)/1024 as Size from CTE
+
+
+
