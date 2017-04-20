9463b29ae63cacee6b65484e1482668afcc29b33
samyan19 <samuel_yanzu@yahoo.co.uk>
2017-04-20 16:26:27 +0100

    Update SPN - SPN Scripts.sql

--- SAM - SPN/SPN - SPN Scripts.sql
+++ SAM - SPN/SPN - SPN Scripts.sql
@@ -6,7 +6,11 @@ If issue with SPN it should appear in the Errorlog.
 setspn -L computername
 setspn -L bestlive\svc_sqlservice
 
---Add SPN
+--Add SPN to standalone
+setspn -A MSSQLSvc/PDC1SQL058.INTERNAL.CLOSEBROTHERS.COM bestlive\svc_sqlservice
+setspn -A MSSQLSvc/PDC1SQL058.INTERNAL.CLOSEBROTHERS.COM:1433 bestlive\svc_sqlservice
+
+--Add SPN to Instance
 setspn -A MSSQLSvc/PL3SQLC01EXT.BESTLIVE.BESTINVEST.CO.UK:EXT01 bestlive\svc_sqlservice
 setspn -A MSSQLSvc/PL3SQLC01INT.BESTLIVE.BESTINVEST.CO.UK:50382 bestlive\svc_sqlservice
 
