e827d775264a92c9881dc96bde61539e8a397e3a
samyan19 <samuel_yanzu@yahoo.co.uk>
2017-05-17 11:24:32 +0100

    Update DATABASE - List Database Files and Free Space.sql

--- SAM - Database/DATABASE - List Database Files and Free Space.sql
+++ SAM - Database/DATABASE - List Database Files and Free Space.sql
@@ -1,19 +1,17 @@
 declare @sptable table ([Database Name] varchar(100),[File Name] varchar(100),[Physical Name] varchar(1000), [Total Size in MB] int, [Available Space In MB] float, Type int)
 
 insert into @sptable
-exec sp_msforeachdb '
+exec sp_MSforeachdb '
 use [?];
 SELECT db_name() as ''Database Name'', name AS ''File Name'' , physical_name AS ''Physical Name'', size/128 AS ''Total Size in MB'',
-
 size/128.0 - CAST(FILEPROPERTY(name, ''SpaceUsed'') AS int)/128.0 AS ''Available Space In MB'',type
-
 FROM sys.database_files'
 
 
 
 select * from @sptable
-WHERE type =0
-and [Physical Name] LIKE 'L%'
+WHERE Type =0
+and [Physical Name] LIKE 'F%'
 ORDER BY [Available Space In MB] DESC
 
 
