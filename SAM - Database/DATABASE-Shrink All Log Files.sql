061dfc445b94d9691b70ae43ffefd186ca622207
samyan19 <samuel_yanzu@yahoo.co.uk>
2017-05-19 15:37:53 +0100

    Update DATABASE-Shrink All Log Files.sql

--- SAM - Database/DATABASE-Shrink All Log Files.sql
+++ SAM - Database/DATABASE-Shrink All Log Files.sql
@@ -1,3 +1,3 @@
-select 'use '+db_name(database_id) +'; dbcc shrinkfile('+name+');'
+select 'use '+QUOTENAME(db_name(database_id)) +'; dbcc shrinkfile('''+name+''');'
 from sys.master_files
 where type_desc='LOG' and database_id not in (1,2,3,4)
