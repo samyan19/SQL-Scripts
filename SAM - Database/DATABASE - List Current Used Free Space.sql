cef9eacd27d5f89ca6627a105db413d862f76f33
samyan19 <samuel_yanzu@yahoo.co.uk>
2017-06-01 10:56:26 +0100

    Update DATABASE - List Current Used Free Space.sql

--- SAM - Database/DATABASE - List Current Used Free Space.sql
+++ SAM - Database/DATABASE - List Current Used Free Space.sql
@@ -1,6 +1,17 @@
-select DB_NAME() as DbName,
-name as FileName,
-cast(size/128.0 as numeric(10,2)) as CurrentSizeMB,
-cast((CAST(FILEPROPERTY(name, 'SpaceUsed') AS INT)/128.0) as numeric(10,2)) AS UsedSpaceMB,
-cast((size/128.0 - CAST(FILEPROPERTY(name, 'SpaceUsed') AS INT)/128.0) as numeric(10,2)) AS FreeSpaceMB
-FROM sys.database_files
+declare @sptable table ([Database Name] varchar(100),[File Name] varchar(100),[Physical Name] varchar(1000), [Current Size MB] int, [Used Space MB] int, [Free Space MB] float, Type int)
+
+insert into @sptable
+exec sp_MSforeachdb '
+use [?];
+SELECT db_name() as ''Database Name'', name AS ''File Name'' , physical_name AS ''Physical Name'', size/128 AS ''Total Size in MB'',
+cast((CAST(FILEPROPERTY(name, ''SpaceUsed'') AS INT)/128.0) as int) AS UsedSpaceMB,
+size/128.0 - cast((CAST(FILEPROPERTY(name, ''SpaceUsed'') AS INT)/128.0) as int)  AS ''Available Space In MB'',type
+FROM sys.database_files'
+
+
+
+select * from @sptable
+WHERE Type =0
+and [Physical Name] LIKE 'F%'
+ORDER BY [Free Space MB] DESC
+
