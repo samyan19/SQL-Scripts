4063bfcfab6e39051c9bc140dcea89d75a81554f
samyan19 <samuel_yanzu@yahoo.co.uk>
2017-08-16 12:37:12 +0100

    Update Check Path for Default Trace.sql

--- SAM - Default Trace/Check Path for Default Trace.sql
+++ SAM - Default Trace/Check Path for Default Trace.sql
@@ -1,8 +1,10 @@
 /*=====================
 
 Five Files Kept
+20 MB each - max of 100MB
 .trc
 
+https://blogs.msdn.microsoft.com/askjay/2012/06/28/default-trace-and-system-health/
 ======================*/
 
 
