af9bf91b0e6481bc96de25c534fc01da75325cde
samyan19 <samuel_yanzu@yahoo.co.uk>
2017-09-13 12:27:54 +0100

    Update and rename CONNECTIONS - Errors in Ring Buffer 2.sql to CONNECTIONS - Errors in Ring Buffer 2 -MAIN.sql

--- SAM - CONNECTIONS/CONNECTIONS - Errors in Ring Buffer 2 -MAIN.sql
+++ SAM - CONNECTIONS/CONNECTIONS - Errors in Ring Buffer 2 -MAIN.sql
@@ -37,9 +37,7 @@ FROM
 FROM sys.dm_os_ring_buffers
 WHERE ring_buffer_type = 'RING_BUFFER_CONNECTIVITY') as tab
 )
-SELECT c.RecordTime,m.[text],* 
+SELECT dateadd(HOUR,1,c.RecordTime),m.[text],* 
 FROM connectivity_ring_buffer c 
     LEFT JOIN sys.messages m ON c.SniConsumerError = m.message_id AND m.language_id = 1033 
 ORDER BY c.RecordTime DESC
-
-
