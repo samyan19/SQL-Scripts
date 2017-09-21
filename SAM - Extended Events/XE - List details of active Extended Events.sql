1de1c8a9c145818171bc6c3272fd5321c6f0aa19
samyan19 <samuel_yanzu@yahoo.co.uk>
2017-09-21 12:34:24 +0100

    Update XE - List details of active Extended Events.sql

--- SAM - Extended Events/XE - List details of active Extended Events.sql
+++ SAM - Extended Events/XE - List details of active Extended Events.sql
@@ -1,3 +1,10 @@
+--list details of all events
+
 select * 
 from sys.server_event_sessions ses
 join sys.server_event_session_fields sesf on ses.event_session_id=sesf.event_session_id
+
+
+--list details of active events 
+
+select * from sys.dm_xe_sessions
