4ea419e8b71c727d31c968bc2875335f0939d390
samyan19 <samuel_yanzu@yahoo.co.uk>
2018-01-25 16:54:44 +0000

    Update INDEXES - Fragmentation Levels 2.sql

--- SAM - INDEX/INDEXES - Fragmentation Levels 2.sql
+++ SAM - INDEX/INDEXES - Fragmentation Levels 2.sql
@@ -1,11 +1,11 @@
-SELECT DISTINCT i.name, ddips.index_depth, ddips.index_level
+SELECT DISTINCT db_name() as database_name, o.name, i.name, ddips.index_depth, ddips.index_level
     , ddips.page_count,(ddips.page_count*8)/1024 as [Size MB], ddips.record_count, ddips.avg_fragmentation_in_percent,i.fill_factor,sp.data_compression
 FROM sys.indexes AS i with (NOLOCK)
 join sys.partitions as sp with (NOLOCK) ON i.object_id=sp.object_id
-Join sys.dm_db_index_physical_stats(DB_ID(), 
-    OBJECT_ID(N'dbo.Mart_Fact_Consumption'), Null, Null, N'Detailed') AS ddips
+Join sys.dm_db_index_physical_stats(DB_ID(),Null, Null, Null, N'Detailed') AS ddips
     ON i.OBJECT_ID = ddips.OBJECT_ID
     And i.index_id = ddips.index_id
+join sys.objects o on i.object_id=o.object_id
 /*WHERE i.name In ('IX_Sales_SalesOrderDetail_SpecialOfferID'
     , 'FIX_Sales_SalesOrderDetail_SpecialOfferID_Filtered'
     , 'PK_SalesOrderDetail_SalesOrderID_SalesOrderDetailID')*/
