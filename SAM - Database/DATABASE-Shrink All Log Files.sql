ba566b23f50d30656c9bdd1775419b4d6377bc49
samyan19 <samuel_yanzu@yahoo.co.uk>
2018-09-27 16:09:36 +0100

    Update DATABASE-Shrink All Log Files.sql

--- SAM - Database/DATABASE-Shrink All Log Files.sql
+++ SAM - Database/DATABASE-Shrink All Log Files.sql
@@ -4,4 +4,4 @@ where type_desc='LOG' and database_id not in (1,2,3,4)
                                                                 
 EXEC sp_msforeachdb 'if DB_ID(''?'')<4 return; alter database [?] set recovery simple; use [?]; checkpoint; dbcc shrinkfile(2);'
                                 
-EXEC sp_msforeachdb 'dbcc shrinkfile(2);'
+EXEC sp_msforeachdb 'USE [?];dbcc shrinkfile(2);'
