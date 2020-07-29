38a1cd27651670979b0485f03a5954ecfd33a8e0
samyan19 <samuel_yanzu@yahoo.co.uk>
2020-07-29 10:20:16 +0100

    Create XE- Extended Events in Azure SQL.sql

---
+++ SAM - Extended Events/XE- Extended Events in Azure SQL.sql
@@ -0,0 +1,152 @@
+/******
+https://docs.microsoft.com/en-us/azure/azure-sql/database/xevent-code-ring-buffer
+
+*******/
+
+GO
+----  Transact-SQL.
+---- Step set 1.
+
+SET NOCOUNT ON;
+GO
+
+
+IF EXISTS
+    (SELECT * FROM sys.objects
+        WHERE type = 'U' and name = 'tabEmployee')
+BEGIN
+    DROP TABLE tabEmployee;
+END
+GO
+
+
+CREATE TABLE tabEmployee
+(
+    EmployeeGuid         uniqueIdentifier   not null  default newid()  primary key,
+    EmployeeId           int                not null  identity(1,1),
+    EmployeeKudosCount   int                not null  default 0,
+    EmployeeDescr        nvarchar(256)          null
+);
+GO
+
+
+INSERT INTO tabEmployee ( EmployeeDescr )
+    VALUES ( 'Jane Doe' );
+GO
+
+---- Step set 2.
+
+
+IF EXISTS
+    (SELECT * from sys.database_event_sessions
+        WHERE name = 'eventsession_gm_azuresqldb51')
+BEGIN
+    DROP EVENT SESSION eventsession_gm_azuresqldb51
+        ON DATABASE;
+END
+GO
+
+
+CREATE
+    EVENT SESSION eventsession_gm_azuresqldb51
+    ON DATABASE
+    ADD EVENT
+        sqlserver.sql_statement_starting
+            (
+            ACTION (sqlserver.sql_text)
+            WHERE statement LIKE '%UPDATE tabEmployee%'
+            )
+    ADD TARGET
+        package0.ring_buffer
+            (SET
+                max_memory = 500   -- Units of KB.
+            );
+GO
+
+---- Step set 3.
+
+
+ALTER EVENT SESSION eventsession_gm_azuresqldb51
+    ON DATABASE
+    STATE = START;
+GO
+
+---- Step set 4.
+
+
+SELECT 'BEFORE_Updates', EmployeeKudosCount, * FROM tabEmployee;
+
+UPDATE tabEmployee
+    SET EmployeeKudosCount = EmployeeKudosCount + 102;
+
+UPDATE tabEmployee
+    SET EmployeeKudosCount = EmployeeKudosCount + 1015;
+
+SELECT 'AFTER__Updates', EmployeeKudosCount, * FROM tabEmployee;
+GO
+
+---- Step set 5.
+
+
+SELECT
+    se.name                      AS [session-name],
+    ev.event_name,
+    ac.action_name,
+    st.target_name,
+    se.session_source,
+    st.target_data,
+    CAST(st.target_data AS XML)  AS [target_data_XML]
+FROM
+               sys.dm_xe_database_session_event_actions  AS ac
+
+    INNER JOIN sys.dm_xe_database_session_events         AS ev  ON ev.event_name = ac.event_name
+        AND CAST(ev.event_session_address AS BINARY(8)) = CAST(ac.event_session_address AS BINARY(8))
+
+    INNER JOIN sys.dm_xe_database_session_object_columns AS oc
+         ON CAST(oc.event_session_address AS BINARY(8)) = CAST(ac.event_session_address AS BINARY(8))
+
+    INNER JOIN sys.dm_xe_database_session_targets        AS st
+         ON CAST(st.event_session_address AS BINARY(8)) = CAST(ac.event_session_address AS BINARY(8))
+
+    INNER JOIN sys.dm_xe_database_sessions               AS se
+         ON CAST(ac.event_session_address AS BINARY(8)) = CAST(se.address AS BINARY(8))
+WHERE
+        oc.column_name = 'occurrence_number'
+    AND
+        se.name        = 'eventsession_gm_azuresqldb51'
+    AND
+        ac.action_name = 'sql_text'
+ORDER BY
+    se.name,
+    ev.event_name,
+    ac.action_name,
+    st.target_name,
+    se.session_source
+;
+GO
+
+---- Step set 6.
+
+
+ALTER EVENT SESSION eventsession_gm_azuresqldb51
+    ON DATABASE
+    STATE = STOP;
+GO
+
+---- Step set 7.
+
+
+ALTER EVENT SESSION eventsession_gm_azuresqldb51
+    ON DATABASE
+    DROP TARGET package0.ring_buffer;
+GO
+
+---- Step set 8.
+
+
+DROP EVENT SESSION eventsession_gm_azuresqldb51
+    ON DATABASE;
+GO
+
+DROP TABLE tabEmployee;
+GO
