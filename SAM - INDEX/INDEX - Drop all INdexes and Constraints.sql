a44b0444668f53f977cc2fa6995d408164ada0db
samyan19 <samuel_yanzu@yahoo.co.uk>
2021-03-31 12:21:21 +0100

    Update INDEX - Drop all INdexes and Constraints.sql

--- SAM - INDEX/INDEX - Drop all INdexes and Constraints.sql
+++ SAM - INDEX/INDEX - Drop all INdexes and Constraints.sql
@@ -1,6 +1,5 @@
 /* 
 o.type filters:
-
 S = System table
 IT = Internal table
 */
@@ -21,15 +20,16 @@ ON t.[schema_id] = s.[schema_id]
 WHERE c.[type] IN ('D','C','F','PK','UQ')
 AND c.type not in ('S','IT')
 ORDER BY c.[type];
-PRINT @sql;
---EXEC sys.sp_executesql @sql;
+--PRINT @sql;
+EXEC sys.sp_executesql @sql;
 
 /* Drop indexes */
-declare @qry nvarchar(1024);
-select @qry = (select 'drop index '+s.name+'.'+o.name+'.'+i.name+';'
+declare @qry nvarchar(max);
+SET @qry = N'';
+select @qry =@qry+N'
+drop index '+QUOTENAME(s.name)+'.'+QUOTENAME(o.name)+'.'+QUOTENAME(i.name)+';'
   from sys.indexes i join sys.objects o on  i.object_id=o.object_id
     join sys.schemas s on o.schema_id=s.schema_id
-  where o.type not in ('S','IT')  and is_primary_key<>1 and index_id>0
-for xml path(''));
-print sp_executesql @qry
---exec sp_executesql @qry
+  where o.type not in ('S','IT')  and is_primary_key<>1 and index_id>0;
+--print @qry;
+exec sp_executesql @qry
