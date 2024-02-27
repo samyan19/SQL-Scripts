7559838b2bd640585286ee24aafb2f0b244fa579
samyan19 <samuel_yanzu@yahoo.co.uk>
2024-02-27 19:23:35 +0000

    Create CONNECTIONS - Find details of active connections.sql

---
+++ SAM - CONNECTIONS/CONNECTIONS - Find details of active connections.sql
@@ -0,0 +1,10 @@
+SELECT   
+    c.session_id, c.net_transport, c.encrypt_option,   
+    c.auth_scheme, s.host_name, s.program_name,   
+    s.client_interface_name, s.login_name, s.nt_domain,   
+    s.nt_user_name, s.original_login_name, c.connect_time,   
+    s.login_time   
+FROM sys.dm_exec_connections AS c  
+JOIN sys.dm_exec_sessions AS s  
+    ON c.session_id = s.session_id  
+WHERE c.session_id = @@SPID;
