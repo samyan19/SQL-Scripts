871924cd5391accee5b5d47d82942a714eb32318
samyan19 <samuel_yanzu@yahoo.co.uk>
2024-08-16 10:31:21 +0100

    Update DISK - CrystalDiskMark Reference.sql

--- SAM - Disk/DISK - CrystalDiskMark Reference.sql
+++ SAM - Disk/DISK - CrystalDiskMark Reference.sql
@@ -1,12 +1,15 @@
 /***
 
-Seq – long, sequential operations. For SQL Server, this is somewhat akin to doing backups or doing table scans of perfectly defragmented data, like a data warehouse.
+https://glennsqlperformance.com/2020/12/13/some-quick-comparative-crystaldiskmark-results-in-2020/
+https://www.brentozar.com/archive/2019/11/how-to-use-crystaldiskmark-7-to-test-your-sql-servers-storage/
 
-512K – random large operations one at a time.  This doesn’t really match up to how SQL Server works.
+Seq â€“ long, sequential operations. For SQL Server, this is somewhat akin to doing backups or doing table scans of perfectly defragmented data, like a data warehouse.
 
-4K – random tiny operations one at a time.  This is somewhat akin to a lightly loaded OLTP server.
+512K â€“ random large operations one at a time.  This doesnâ€™t really match up to how SQL Server works.
 
-4K QD32 – random tiny operations, but many done at a time.  This is somewhat akin to an active OLTP server.
+4K â€“ random tiny operations one at a time.  This is somewhat akin to a lightly loaded OLTP server.
+
+4K QD32 â€“ random tiny operations, but many done at a time.  This is somewhat akin to an active OLTP server.
 
 
 ***/
